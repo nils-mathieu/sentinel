38e424d92db63443a11a74b83f29c74505b4c938
nils-mathieu <nils.mathieu.contact@gmail.com>
2022-10-06 02:41:51 +0200

    add the `.display()` method to the `SSlice<i8>` type

--- src/display.rs
+++ src/display.rs
@@ -11,14 +11,29 @@ impl SSlice<u8, Null> {
     ///
     /// [`REPLACEMENT_CHARACTER`]: core::char::REPLACEMENT_CHARACTER
     #[inline(always)]
-    pub fn display(&self) -> impl fmt::Display {
+    pub fn display(&self) -> &DisplayCStr {
+        unsafe { &*(self as *const Self as *const DisplayCStr) }
+    }
+}
+
+impl SSlice<i8, Null> {
+    /// An implementation of [`fmt::Display`] and [`fmt::Debug`] for a [`SSlice<i8>`] type.
+    ///
+    /// When an invalid character is found, the [`REPLACEMENT_CHARACTER`] is displayed instead.
+    ///
+    /// [`REPLACEMENT_CHARACTER`]: core::char::REPLACEMENT_CHARACTER
+    #[inline(always)]
+    pub fn display(&self) -> &DisplayCStr {
+        // SAFETY:
+        //  We'll simply note that we are transmuting an [i8] slice int an [u8] slice. That
+        //  transmutation is always safe.
         unsafe { &*(self as *const Self as *const DisplayCStr) }
     }
 }
 
 /// Implements [`fmt::Display`] [`fmt::Debug`] for a [`CStr`].
 #[repr(transparent)]
-struct DisplayCStr(SSlice<u8, Null>);
+pub struct DisplayCStr(SSlice<u8, Null>);
 
 impl fmt::Display for DisplayCStr {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
@@ -31,12 +46,14 @@ impl fmt::Display for DisplayCStr {
 
 impl fmt::Debug for DisplayCStr {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        f.write_str("\"")?;
         for opt in DecodeUtf8(self.0.iter().copied()) {
             fmt::Display::fmt(
                 &opt.unwrap_or(char::REPLACEMENT_CHARACTER).escape_debug(),
                 f,
             )?;
         }
+        f.write_str("\"")?;
         Ok(())
     }
 }
